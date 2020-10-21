a83f7eb39ae2aef924faa1d17974401811ab4cf4
balazsnemeth <balazs.nemeth@attrecto.com>
2020-10-21 11:42:49 +0200

    Activity lifecycle

--- app/src/main/java/com/attrecto/academy/MainActivity.kt
+++ app/src/main/java/com/attrecto/academy/MainActivity.kt
@@ -2,6 +2,7 @@ package com.attrecto.academy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LiveData
@@ -9,8 +10,12 @@ import androidx.lifecycle.MutableLiveData
 import com.attrecto.academy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "ALIFECYCLE"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = this
@@ -23,5 +28,25 @@ class MainActivity : AppCompatActivity() {
     fun showToast(name : String) {
         Toast.makeText(this, getString(R.string.hello_world, name), Toast.LENGTH_SHORT).show()
         this.name.value = ""
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
     }
 }
