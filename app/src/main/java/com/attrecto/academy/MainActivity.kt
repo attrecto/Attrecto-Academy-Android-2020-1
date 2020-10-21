a02ce57df4d617f7cd9d4ebeaf27180fac717503
balazsnemeth <balazs.nemeth@attrecto.com>
2020-10-21 08:31:50 +0200

    Hello toast

--- app/src/main/java/com/attrecto/academy/MainActivity.kt
+++ app/src/main/java/com/attrecto/academy/MainActivity.kt
@@ -2,10 +2,16 @@ package com.attrecto.academy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toast_button.setOnClickListener {
+            Toast.makeText(this, R.string.hello_world, Toast.LENGTH_SHORT).show()
+        }
     }
 }
