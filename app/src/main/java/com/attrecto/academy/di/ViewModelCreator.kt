bea08fbd0030d847e4c9a3c2eb0988e1104c2f80
balazsnemeth <balazs.nemeth@attrecto.com>
2020-10-28 12:35:27 +0100

    resolve conflict

--- app/src/main/java/com/attrecto/academy/di/ViewModelCreator.kt
+++ app/src/main/java/com/attrecto/academy/di/ViewModelCreator.kt
@@ -1,7 +1,7 @@
 package com.attrecto.academy.di
 
 import android.app.Application
-import com.attrecto.academy.screen.detail.MainViewModel
+import com.attrecto.academy.screen.main.MainViewModel
 
 fun createMainViewModel(application: Application) =
     MainViewModel(application, createMovieHeaderRepository())
